83fb30656719519a60b058e4c51bb9825529162a
nwdxlgzs <2283566187@qq.com>
2021-10-04 22:24:58 +0800

    upload datas

--- upload2git.bat
+++ upload2git.bat
@@ -2,6 +2,7 @@
 echo 上传中。
 git add .
 git commit -m "upload datas"
+git remote rm origin
 git remote add origin git@github.com:nwdxlgzs/datas-lua-webverify.git
 git push -u origin master
 echo 执行完毕，退出请按任意键。
