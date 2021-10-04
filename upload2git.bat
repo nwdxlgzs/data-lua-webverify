6e43c3f9ce0d3d491b52ce073fa3595cbed3d3d2
nwdxlgzs <2283566187@qq.com>
2021-10-04 22:30:14 +0800

    数据更新

--- upload2git.bat
+++ upload2git.bat
@@ -1,7 +1,5 @@
 @echo off
 echo 上传中。
-git add . ||echo 执行完毕，退出请按任意键。
-git commit -m "数据更新" ||echo 执行完毕，退出请按任意键。
-git push -u git@github.com:nwdxlgzs/datas-lua-webverify.git master ||echo 执行完毕，退出请按任意键。
-
-pause
+git add . ||pause
+git commit -m "数据更新" ||pause
+git push -u git@github.com:nwdxlgzs/datas-lua-webverify.git master ||pause
