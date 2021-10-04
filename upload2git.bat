2fcbf04c9e828b75801b8fcf8d79960f91f69fba
nwdxlgzs <2283566187@qq.com>
2021-10-04 22:20:14 +0800

    upload datas

--- upload2git.bat
+++ upload2git.bat
@@ -1,7 +1,10 @@
 @echo off
-
 echo git add .
 git add .
 echo git commit -m ^"upload datas^"
 git commit -m "upload datas"
+echo git remote add origin git@github.com:nwdxlgzs/datas-lua-webverify.git
+git remote add origin git@github.com:nwdxlgzs/datas-lua-webverify.git
+echo git push -u origin master
+git push -u origin master
 pause
