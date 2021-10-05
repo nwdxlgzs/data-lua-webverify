06e2fdfcffd718a7dc1ce8d5bdf2612204a5cef7
nwdxlgzs <2283566187@qq.com>
2021-10-05 19:42:10 +0800

    file update.

--- upload2git.bat
+++ upload2git.bat
@@ -1,10 +1,11 @@
 @echo off
+echo wscript.echo DateDiff("s", "01/01/1970 00:00:00", Now())>sjc.vbs
+for /f %%i in ('cscript sjc.vbs /nologo') do echo %%i>sjc.vbs
+ren sjc.vbs ping
 git add . ||pause
 git commit -m "file update." ||pause
 git remote rm origin
 git remote add origin git@github.com:nwdxlgzs/data-lua-webverify.git
 git push -u git@github.com:nwdxlgzs/data-lua-webverify.git master ||pause
-echo wscript.echo DateDiff("s", "01/01/1970 00:00:00", Now())>sjc.vbs
-for /f %%i in ('cscript sjc.vbs /nologo') do echo %%i>sjc.vbs
-ren sjc.vbs ping
+
 TIMEOUT /T 10
