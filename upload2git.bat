036dceca67dddfa66212c6a10379efbe8fbe6c37
nwdxlgzs <2283566187@qq.com>
2021-10-05 19:30:44 +0800

    file update.

--- upload2git.bat
+++ upload2git.bat
@@ -2,4 +2,10 @@
 git add . ||pause
 git commit -m "file update." ||pause
 git push -u git@github.com:nwdxlgzs/datas-lua-webverify.git master ||pause
+cur_sec_and_ns=`date '+%s-%N'`
+Ncur_ns=`date '+%N'`
+cur_sec=${cur_sec_and_ns%-*}
+cur_ns=${cur_sec_and_ns##*-}
+cur_timestamp=$((cur_sec*1000+cur_ns/1000000))
+echo cur_timestamp
 TIMEOUT /T 10
